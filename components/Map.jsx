96a731ee6e334738a01978ba98dee465867cff30
AyanPaL7876 <ayanpal260@gmail.com>
2025-03-02 13:12:26 +0530

    Enhance Map component with zoom functionality and user location tracking

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,24 +1,21 @@
-import { View, Text } from 'react-native'
-import React, { useEffect } from 'react'
-import MapboxGL, { MapView } from "@rnmapbox/maps";
+import { View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import MapboxGL, { Camera, LocationPuck, MapView } from "@rnmapbox/maps";
 import * as Location from 'expo-location';
 import Mapbox from '@rnmapbox/maps';
 
 const Map = () => {
+  const [zoom, setZoom] = useState(2); // Start with low zoom level
+
   useEffect(() => {
     const initializeMap = async () => {
       try {
-        // Initialize Mapbox
         Mapbox.setAccessToken(process.env.EXPO_PUBLIC_MAPBOX_API_KEY);
-        
-        // Request location permissions
-        // const { status } = await Location.requestForegroundPermissionsAsync();
-        // if (status !== 'granted') {
-        //   console.log('Permission to access location was denied');
-        //   return;
-        // }
 
-        // MapboxGL.setTelemetryEnabled(false);
+        // Delay zoom animation to ensure map is rendered
+        setTimeout(() => {
+          setZoom(14); // Target zoom level
+        }, 500); // Delay before animation starts
       } catch (error) {
         console.error('Error initializing map:', error);
       }
@@ -27,12 +24,24 @@ const Map = () => {
     initializeMap();
   }, []);
 
+  // styleURL="mapbox://styles/mapbox/standard-satellite"
+
   return (
-    // <View style={{ flex: 1 }}>
-    //   <Text>Map</Text>
-    // </View>
-    <MapView style={{flex:1}}/>
+    <MapView 
+    style={{ flex: 1 }} 
+    zoomEnabled={true} 
+    styleURL='mapbox://styles/mapbox/navigation-night-v1'>
+      <Camera
+        followUserLocation
+        followZoomLevel={14}
+        centerCoordinate
+      />
+      <LocationPuck 
+      puckBearingEnabled puckBearing='heading'
+      pulsing={{isEnabled: true, color: '#60A5FA'}}
+      />
+    </MapView>
   )
 }
 
-export default Map
+export default Map;
