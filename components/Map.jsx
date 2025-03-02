483df2cf953f0b9525139bd7d097c372eb4a37b1
AyanPaL7876 <ayanpal260@gmail.com>
2025-03-03 02:13:20 +0530

    Add alert points to Map component with clustering and custom icons

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,21 +1,19 @@
 import { View } from 'react-native'
 import React, { useEffect, useState } from 'react'
-import MapboxGL, { Camera, LocationPuck, MapView } from "@rnmapbox/maps";
+import MapboxGL, { Camera, LocationPuck, MapView, ShapeSource, SymbolLayer, Images, CircleLayer } from "@rnmapbox/maps";
 import * as Location from 'expo-location';
 import Mapbox from '@rnmapbox/maps';
+import { featureCollection, point } from '@turf/helpers';
+import alertPoint from '../data/alertPoint.json';
+import alertPin from '../assets/images/alert.png';
 
 const Map = () => {
-  const [zoom, setZoom] = useState(2); // Start with low zoom level
+  const points = alertPoint.map(alert => point([alert.long, alert.lat]));
 
   useEffect(() => {
     const initializeMap = async () => {
       try {
         Mapbox.setAccessToken(process.env.EXPO_PUBLIC_MAPBOX_API_KEY);
-
-        // Delay zoom animation to ensure map is rendered
-        setTimeout(() => {
-          setZoom(14); // Target zoom level
-        }, 500); // Delay before animation starts
       } catch (error) {
         console.error('Error initializing map:', error);
       }
@@ -23,6 +21,8 @@ const Map = () => {
 
     initializeMap();
   }, []);
+
+  // const scooterFeatures = featureCollection(points);
 
   // styleURL="mapbox://styles/mapbox/standard-satellite"
 
@@ -40,6 +40,49 @@ const Map = () => {
       puckBearingEnabled puckBearing='heading'
       pulsing={{isEnabled: true, color: '#60A5FA'}}
       />
+
+      <ShapeSource 
+      id='alerts' 
+      cluster 
+      shape={featureCollection(points)}
+      onPress={(event) => {console.log(JSON.stringify(event, null, 2))}}
+      >
+        <SymbolLayer 
+        id='cluster-count'
+        style={{
+          textField: ['get', 'point_count'],
+          textSize: 12,
+          textColor: '#ffffff',
+          textPitchAlignment: 'map',
+        }}
+        />
+        <CircleLayer
+          id = "clusters"
+          belowLayerID='cluster-count'
+          filter={['has', 'point_count']}
+          style={{
+            circlePitchAlignment: 'map',
+            circleColor: '#FCA5A5',
+            circleRadius: 20,
+            circleOpacity: 0.7,
+            circleStrokeWidth: 2,
+            circleStrokeColor: '#EF4444',
+          }}
+        />
+        <SymbolLayer 
+        id='alerts-icons'
+        filter={['!',['has', 'point_count']]} 
+        style={{ 
+          iconImage: 'alertPin', 
+          iconSize: 0.07,
+          iconAllowOverlap: true,
+          iconAnchor: 'bottom',
+        }} />
+        <Images 
+        images={{ alertPin: alertPin }} 
+        />
+        
+      </ShapeSource>
     </MapView>
   )
 }
